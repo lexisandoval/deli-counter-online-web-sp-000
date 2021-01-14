65e63eba04725646642e31b2a7027c5ee6699f8f
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-14 16:59:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,13 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length < 1
     puts "The line is currently empty."
+  else
+    puts "The line is currently: "
+    counter = 0
+    new
+    katz_deli.each do |x|
+      new << x
+      counter += 1
+      puts "#{counter}. #{x}"
   end
 end
